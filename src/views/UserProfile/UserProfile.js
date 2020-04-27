50f7e5e783b8225b04f8c6ea14ffd1b6813b47b8
Maaz Mehtab <maaz.mehtab@waqartech.com>
2020-04-27 16:54:05 +0500

    ProfileUI

--- src/views/UserProfile/UserProfile.js
+++ src/views/UserProfile/UserProfile.js
@@ -1,20 +1,16 @@
-import React from "react";
-// @material-ui/core components
+import React, { useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import InputLabel from "@material-ui/core/InputLabel";
-// core components
 import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
-import CustomInput from "components/CustomInput/CustomInput.js";
 import Button from "components/CustomButtons/Button.js";
 import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
 import CardAvatar from "components/Card/CardAvatar.js";
 import CardBody from "components/Card/CardBody.js";
 import CardFooter from "components/Card/CardFooter.js";
-
+import CssTextField from 'components/CssTextField/CssTextField.js';
 import avatar from "assets/img/faces/marc.jpg";
-
+import * as util from "../../helper/Utilities";
 const styles = {
   cardCategoryWhite: {
     color: "rgba(255,255,255,.62)",
@@ -34,10 +30,73 @@ const styles = {
   }
 };
 
-const useStyles = makeStyles(styles);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  cardCategoryWhite: {
+    "&,& a,& a:hover,& a:focus": {
+      color: "rgba(255,255,255,.62)",
+      margin: "0",
+      fontSize: "14px",
+      marginTop: "0",
+      marginBottom: "0"
+    },
+    "& a,& a:hover,& a:focus": {
+      color: "#FFFFFF"
+    }
+  },
+  cardTitleWhite: {
+    color: "#FFFFFF",
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: "3px",
+    textDecoration: "none",
+    "& small": {
+      color: "#777",
+      fontSize: "65%",
+      fontWeight: "400",
+      lineHeight: "1"
+    }
+  },
+  input1: {
+    height: 10,
+
+  }
+}));
 
 export default function UserProfile() {
   const classes = useStyles();
+  const [values, setValues] = React.useState({
+    userName: '',
+    email: '',
+    phone: '',
+    country: '',
+  });
+  const handleChange = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
+  };
+  useEffect(() => {
+    getUserDetails()
+  }, [])
+  const getUserDetails = async ()=> {
+    let userObj = await util.localStorage_GetKey('user')
+    userObj = JSON.parse(userObj)
+    console.log("userObj",userObj);
+    // setValues({
+    //   ...values,
+    //   userName: userObj.userName,
+    //   email: userObj.email,
+    //   country: userObj.country,
+    //   phone: userObj.phone,
+    // });
+
+  }
   return (
     <div>
       <GridContainer>
@@ -49,102 +108,52 @@ export default function UserProfile() {
             </CardHeader>
             <CardBody>
               <GridContainer>
-                <GridItem xs={12} sm={12} md={5}>
-                  <CustomInput
-                    labelText="Company (disabled)"
-                    id="company-disabled"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
+                <GridItem xs={12} sm={12} md={6}>
+                  <CssTextField
+                    value={values.userName}
+                    onChange={handleChange}
+                    label="User Name"
+                    name="userName"
+                    type="name"
+                  />
+                </GridItem>
+                <GridItem xs={12} sm={12} md={6}>
+                  <CssTextField
+                    value={values.email}
+                    onChange={handleChange}
+                    label="Email"
+                    name="email"
+                    type="name"
                     inputProps={{
                       disabled: true
                     }}
                   />
                 </GridItem>
-                <GridItem xs={12} sm={12} md={3}>
-                  <CustomInput
-                    labelText="Username"
-                    id="username"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="Email address"
-                    id="email-address"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </GridItem>
+
               </GridContainer>
               <GridContainer>
                 <GridItem xs={12} sm={12} md={6}>
-                  <CustomInput
-                    labelText="First Name"
-                    id="first-name"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
+                  <CssTextField
+                    value={values.phone}
+                    onChange={handleChange}
+                    label="Phone"
+                    name="phone"
+                    type="name"
                   />
                 </GridItem>
                 <GridItem xs={12} sm={12} md={6}>
-                  <CustomInput
-                    labelText="Last Name"
-                    id="last-name"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
+                  <CssTextField
+                    value={values.country}
+                    onChange={handleChange}
+                    label="Country"
+                    name="country"
+                    type="name"
                   />
+
                 </GridItem>
               </GridContainer>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="City"
-                    id="city"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="Country"
-                    id="country"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="Postal Code"
-                    id="postal-code"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </GridItem>
-              </GridContainer>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={12}>
-                  <InputLabel style={{ color: "#AAAAAA" }}>About me</InputLabel>
-                  <CustomInput
-                    labelText="Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo."
-                    id="about-me"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                    inputProps={{
-                      multiline: true,
-                      rows: 5
-                    }}
-                  />
-                </GridItem>
-              </GridContainer>
+
+
             </CardBody>
             <CardFooter>
               <Button color="primary">Update Profile</Button>
@@ -159,15 +168,8 @@ export default function UserProfile() {
               </a>
             </CardAvatar>
             <CardBody profile>
-              <h6 className={classes.cardCategory}>CEO / CO-FOUNDER</h6>
-              <h4 className={classes.cardTitle}>Alec Thompson</h4>
-              <p className={classes.description}>
-                Don{"'"}t be scared of the truth because we need to restart the
-                human foundation in truth And I love you like Kanye loves Kanye
-                I love Rick Owens’ bed design but the back is...
-              </p>
               <Button color="primary" round>
-                Follow
+                Change Profile
               </Button>
             </CardBody>
           </Card>
